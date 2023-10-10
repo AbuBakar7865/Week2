c5cd7c169945520cd91a78f8ac31ca0ec591d39b
c3672431 <a.amjad8875@student.leedsbeckett.ac.uk>
2023-10-10 12:18:29 +0100

    i changed the readme file

---
+++ week2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    print('Hello, World')
+    total = 100
+
+    print(total)
+    total = total + 99
+    print("The total is now", total)
+    total += 99
+    print("The total is now", total)
+    total *= 100
+    print("the total is now", total)
+
+    #task2
+    total -= 1
+    print("The total is", total)
+
+    total *= 4
+    print("The total is", total)
+
+    total /= 2
+    print("The total is", total)
+
+
+
