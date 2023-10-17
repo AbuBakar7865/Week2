0c73907bbee28cdb9aad2dabf3ad81c973774f1a
c3672431 <a.amjad8875@student.leedsbeckett.ac.uk>
2023-10-17 13:16:53 +0100

    working on phyton console week 3

--- week3.py
+++ week3.py
@@ -1,1 +1,16 @@
-name = "Sir Robin of Camelot"
+week 3 practical 1
+ageStr = Input("enter your age")
+age = int(ageStr)
+
+if age > 100:
+    print("you are very old")
+    print("well done!")
+elif age > 80:
+    print("you are fairly old")
+    print("pretty good!")
+elif age > 40:
+    print("you are middle aged")
+    print("never mind")
+else:
+    print("you are not very old - yet" )
+
